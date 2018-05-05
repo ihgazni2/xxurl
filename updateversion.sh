2052d2a7a425469be9306e313bd23ffe60dbfbd3
ihgazni2 <286264978@qq.com>
2018-05-06 00:12:44 +0800

    remove old build

--- updateversion.sh
+++ updateversion.sh
@@ -1,13 +1,13 @@
 #
-pip3 uninstall edict
+pip3 uninstall xxurl
 git rm -r dist
 git rm -r build
 #
-git rm -r edict.egg-info
+git rm -r xxurl.egg-info
 rm -r dist
 rm -r build
 #
-rm -r edict.egg-info
+rm -r xxurl.egg-info
 git add .
 git commit -m "remove old build"
 git push origin master
