b64bd194bf0362bb7abc2c494ae8dad15c82fbbf
ihgazni2 <286264978@qq.com>
2018-05-06 00:04:50 +0800

    remove old build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
       #name ="main"
       name="xxurl",
-      version = "0.1",
+      version = "0.2",
       description="handle url, javascript-style APIs",
       author="dapeli",
       #https://github.com/ihgazni2/main
